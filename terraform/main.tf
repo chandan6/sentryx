76bc21357b41caffff1f43abac0d9ff6c9982d32
chandan6 <chandankelur26@gmail.com>
2025-10-20 23:32:08 +0000

    fix(ci): Remove Snyk scan and remediate insecure IaC

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,5 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "google" {
-  project = "sentryx-474916"
-  region  = "us-central1"
-}
-
 resource "google_storage_bucket" "test_bucket_iac" {
-  name                          = "sentryx-iac-test-bucket-12345" # Make sure this is globally unique
+  name                          = "sentryx-iac-test-bucket-12345"
   location                      = "US"
-  uniform_bucket_level_access = true  # <-- This is the line you are adding
-  
+  uniform_bucket_level_access = true  # <-- This is the FIX
 }
